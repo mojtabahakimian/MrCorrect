0043b83bab52ba539fe1aaea7bd74b6069c48670
HakimianMohamad-2005 <mohamadhakimianmanager@gmail.com>
2025-07-29 09:55:50 +0330

    اضافه شدن گزارشات ویزیتوری به منوی تصویری

--- Prg_UI/Wins/ThePages/PG_VISITORY.xaml.cs
+++ Prg_UI/Wins/ThePages/PG_VISITORY.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Prg_UI.Functions;
+﻿using Functions;
+using Prg_UI.Functions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -21,7 +22,52 @@ namespace Wins.ThePages
 
         private void Image_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            
+            CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.F_MENU_DATE_VISIT_VISITDLV, null /*DEFAULT OWNER MAIN*/);
+        }
+
+        private void Image_PreviewMouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.F_MENU_DATE_VISIT_VISITKAL, null /*DEFAULT OWNER MAIN*/);
+        }
+
+        private void Image_PreviewMouseDown_2(object sender, MouseButtonEventArgs e)
+        {
+            CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.F_MENU_DATE_VISIT_VISITKALMA, null /*DEFAULT OWNER MAIN*/);
+        }
+
+        private void Image_PreviewMouseDown_3(object sender, MouseButtonEventArgs e)
+        {
+            CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.F_MENU_DATE_VISIT_VISITKALMA, null /*DEFAULT OWNER MAIN*/);
+        }
+
+        private void Image_PreviewMouseDown_4(object sender, MouseButtonEventArgs e)
+        {
+            CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.F_MENU_DATE_VISIT_VISITONE, null /*DEFAULT OWNER MAIN*/);
+        }
+
+        private void Image_PreviewMouseDown_5(object sender, MouseButtonEventArgs e)
+        {
+            CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.F_MENU_AJNAS, null /*DEFAULT OWNER MAIN*/);
+        }
+
+        private void Image_PreviewMouseDown_6(object sender, MouseButtonEventArgs e)
+        {
+            CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.VISITOR_GOL_REP, null /*DEFAULT OWNER MAIN*/);
+        }
+
+        private void Image_PreviewMouseDown_7(object sender, MouseButtonEventArgs e)
+        {
+            CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.VISITOR_GOL_GRP_REP_MAR, null /*DEFAULT OWNER MAIN*/);
+        }
+
+        private void Image_PreviewMouseDown_8(object sender, MouseButtonEventArgs e)
+        {
+            CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.VISITOR_GOL_REP_MAR, null /*DEFAULT OWNER MAIN*/);
+        }
+
+        private void Image_PreviewMouseDown_9(object sender, MouseButtonEventArgs e)
+        {
+            CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.VISITOR_GOL_GRP_REP, null /*DEFAULT OWNER MAIN*/);
         }
     }
 }
