7e0d4305adb1ac196e1cc2826e1bf5e890f20d21
mojtabahakimian <mojtabahakimian1380@gmail.com>
2025-09-02 13:17:15 +0330

    F12 رفع اشکال تا 70 درصد

--- Prg_UI/Wins/USER_LOGIN.xaml.cs
+++ Prg_UI/Wins/USER_LOGIN.xaml.cs
@@ -361,8 +361,7 @@ namespace Prg_UI.Wins
             CL_Generaly.VAHED_OF_USER = 1;
             Baseknow.UGRP = "1";
 
-            new SQLSTATEFORM().Show();
-            //new HEAD_SERCH_MAIN_ADVANC().Show();
+            new HEAD_SERCH_MAIN_ADVANC().Show();
 
             //CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.paymentformorder, this);
             //CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.HEAD_LST_FROOSH_AUTO_DETECT, this );
