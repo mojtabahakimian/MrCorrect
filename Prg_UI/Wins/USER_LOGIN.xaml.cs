0b96f5d115094578ec1b5eb86b73130fa9e142dd
Administrator <Administrator@MERCEDES>
2026-01-22 05:29:20 +0330

    1

--- Prg_UI/Wins/USER_LOGIN.xaml.cs
+++ Prg_UI/Wins/USER_LOGIN.xaml.cs
@@ -379,11 +379,11 @@ namespace Prg_UI.Wins
 
             //dotnet publish Prg_UI.csproj -c Release -r win-x64 --self-contained false /p:PublishSingleFile=true -o E:\prg\PublishedFiles; explorer E:\prg\PublishedFiles
 
-            //new WinBase().Show();
+            new WinBase().Show();
             //new Prg_UI.Wins.WinMenus.HESABDARI.PGET_HED().Show();
             //new TR_FACOTRLST(5).Show();
             //new TR_ANBGRD_LST().Show();
-            CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.MOGUDI_SEARCH_MAIN, this);
+            //CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.MOGUDI_SEARCH_MAIN, this);
 
             //new F_MENU_GOZARESH_FROOSH("FR").ShowDialog();
 
