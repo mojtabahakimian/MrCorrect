c5ca2359669cd4d278b2a247f3effc5d3a5047ba
Administrator <Administrator@MERCEDES>
2026-02-28 09:28:57 +0330

    Begin ایجاد سال مالی

---
+++ Prg_UI/Wins/WinMenus/HESABDARI/WIN_F_NEWYEAR.xaml.cs
@@ -0,0 +1,190 @@
+﻿using Interfaces;
+using MaterialDesignThemes.Wpf;
+using Microsoft.Data.SqlClient;
+using Prg_Proccessy.FUNCTIONS;
+using Prg_Proccessy.MODELS;
+using Prg_Proccessy.SQLMODELS;
+using Prg_SendInvoice.CNNMANAGER;
+using Prg_UI.Functions;
+using Prg_UI.HelperWins;
+using Prg_UI.UiTools;
+using Syncfusion.Data.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Threading;
+namespace Prg_UI.Wins.WinMenus.HESABDARI
+{
+    /// <summary>
+    /// Interaction logic for WIN_F_NEWYEAR.xaml
+    /// </summary>
+    public partial class WIN_F_NEWYEAR : Window
+    {
+        public WIN_F_NEWYEAR()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        #region Header Window Begin
+        //Header Window Begin
+        private void Btn_Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Btn_Max_Click(object sender, RoutedEventArgs e)
+        {
+            PackIcon? packIcon = Btn_Max.Content as PackIcon;
+
+            switch (WindowState)
+            {
+                case WindowState.Maximized:
+                    WindowState = WindowState.Normal;
+                    if (packIcon != null)
+                        packIcon.Kind = PackIconKind.WindowMaximize;
+                    break;
+                case WindowState.Normal:
+                    WindowState = WindowState.Maximized;
+                    if (packIcon != null)
+                        packIcon.Kind = PackIconKind.WindowRestore;
+                    break;
+            }
+        }
+        private void Btn_Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void TitleDrawBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+            if (e.ClickCount == 2)
+            {
+                Btn_Max_Click(null, null);
+            }
+        }
+        //Header Window End;
+        #endregion
+
+        private readonly CL_CCNNMANAGER dbms = new CL_CCNNMANAGER();
+
+        UniversControl universControl = new UniversControl();
+
+        public bool NowIsReady { get; private set; }
+        public double? NUMBER_TO_OPEN { get; set; }
+        public bool ChangeIsHappend { get; private set; }
+
+        private bool _bl;
+        public bool AllowDeletions
+        {
+            get { return _bl; }
+            set
+            {
+
+                _bl = value;
+
+                // Get the window handle
+                IntPtr handle = new WindowInteropHelper(this).Handle;
+
+                // Only proceed if the handle is valid
+                if (handle != IntPtr.Zero)
+                {
+                    CL_LMethods.AllowDeletions(this.GetType().Name, _bl, handle);
+                }
+                else
+                {
+                    // Defer the operation until the window is fully rendered
+                    this.Dispatcher.BeginInvoke(new Action(() => {
+                        // Try again after the window is fully initialized
+                        IntPtr newHandle = new WindowInteropHelper(this).Handle;
+                        if (newHandle != IntPtr.Zero)
+                        {
+                            CL_LMethods.AllowDeletions(this.GetType().Name, _bl, newHandle);
+                        }
+                    }), System.Windows.Threading.DispatcherPriority.Loaded);
+                }
+            }
+        }
+        private bool ican;
+        public bool AllowEdits
+        {
+            get { return ican; }
+            set
+            {
+                ican = value;
+
+                //TextBox.IsReadOnly = !ican;
+                //ComboBox.IsEnabled = ican;
+            }
+        }
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            NowIsReady = true;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CL_HESABDARI.AMALIYAT_USER(this.GetType().Name);
+
+            //CL_HESABDARI.SETSECURITY(this.GetType().Name, "", new WindowInteropHelper(this).Handle, this.GetType().Name);
+            //if (!this.IsLoaded)
+            //{
+            //    this.Close();
+            //    return;
+            //}
+
+            FILL_ALL_COMBOBOXES();
+        }
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key is Key.Enter && Keyboard.Modifiers == ModifierKeys.None)
+            {
+                e.Handled = true;
+
+                CL_LMethods.SendKey_US(Key.Tab);
+            }
+
+            // اگر کلیدی که باعث تغییر داده نمی‌شود فشرده شده، نادیده بگیرید
+            var nonDataKeys = new[]
+            {
+                Key.Enter, Key.Tab, Key.LeftShift, Key.RightShift,
+                Key.CapsLock, Key.Left, Key.Right, Key.Up, Key.Down,
+                Key.LeftAlt, Key.RightAlt, Key.LeftCtrl, Key.RightCtrl,
+                Key.F1, Key.F2, Key.F3, Key.F4, Key.F5, Key.F6,
+                Key.F7, Key.F8, Key.F9, Key.F10, Key.F11, Key.F12,
+                Key.Escape, Key.Insert, Key.Home, Key.End,
+                Key.PageUp, Key.PageDown
+            };
+            if (!nonDataKeys.Contains(e.Key))
+            {
+                var focused = Keyboard.FocusedElement as DependencyObject;
+                if (focused != null && (CL_LMethods.IsInside<TextBoxBase>(focused) || CL_LMethods.IsInside<ComboBox>(focused) || CL_LMethods.IsInside<CheckBox>(focused)))
+                {
+                    ChangeIsHappend = true;
+                }
+                else
+                {
+                    var focusedElement = Keyboard.FocusedElement;
+                    if (focusedElement is Xceed.Wpf.Toolkit.MaskedTextBox)
+                    {
+                        ChangeIsHappend = true;
+                    }
+                }
+            }
+        }
+        private void FILL_ALL_COMBOBOXES()
+        {
+            //COMBOHESAB.ItemsSource = dbms.DoGetDataSQL<HESAB_CMB_MODEL>($"SELECT hes, NAME FROM CUST_HESAB ORDER BY hes").ToList();
+        }
+    }
+}
