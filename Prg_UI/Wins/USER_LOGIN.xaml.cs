f2454523b26c86f558613dc46af752a39059bbe4
Administrator <Administrator@MERCEDES>
2026-03-09 00:12:26 +0330

    طراحی جدید

--- Prg_UI/Wins/USER_LOGIN.xaml.cs
+++ Prg_UI/Wins/USER_LOGIN.xaml.cs
@@ -373,11 +373,11 @@ namespace Prg_UI.Wins
             Baseknow.UGRP = "1";
             //CL_Generaly.VAHED_OF_USER = 20; //دپارتمان DEPARTEMAN یزد ویزیتوری
 
-            //new WinBase().Show();
+            new WinBase().Show();
             //new ZASESABBEESAB().Show();
             //new F_MENU_DATE("CROS").Show();
             //CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.F_MENU_KOL_MOIN_TAFZIL, this);
-            new WIN_F_NEWYEAR().Show();
+            //new WIN_F_NEWYEAR().Show();
             //CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.paymentformorder, this, 1642d);
             //dotnet publish Prg_UI.csproj -c Release -r win-x64 --self-contained false /p:PublishSingleFile=true -o E:\prg\PublishedFiles; explorer E:\prg\PublishedFiles
 
