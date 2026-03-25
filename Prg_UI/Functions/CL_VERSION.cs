7cb3c61f91cd5f18920e92f893de4559097f4801
Administrator <Administrator@MERCEDES>
2026-03-26 03:01:56 +0330

    ver

--- Prg_UI/Functions/CL_VERSION.cs
+++ Prg_UI/Functions/CL_VERSION.cs
@@ -11,7 +11,7 @@ namespace Functions
     public static class CL_VERSION
     {
         //public static string MrCorrectFullVersion { get; } = "Version 1.0.0.327 Date : 1404/09/24";
-        public static string MrCorrectFullVersion { get; } = "Version 1.0.0.381 Date : 1405/01/04";
+        public static string MrCorrectFullVersion { get; } = "Version 1.0.0.382 Date : 1405/01/05";
 
         /// <summary>
         /// Checks if the current application version is valid and greater than or equal to the version stored in the database.
