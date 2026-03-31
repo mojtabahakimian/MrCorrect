1a6e6ec32f76e618a0c60c07e65b0d245c6db126
Administrator <Administrator@MERCEDES>
2026-03-31 18:52:47 +0330

    version

--- Prg_UI/Wins/USER_LOGIN.xaml.cs
+++ Prg_UI/Wins/USER_LOGIN.xaml.cs
@@ -370,9 +370,9 @@ namespace Prg_UI.Wins
             Baseknow.UGRP = "1";
             //CL_Generaly.VAHED_OF_USER = 20; //دپارتمان DEPARTEMAN یزد ویزیتوری
 
-            new WinBase().Show();
+            //new WinBase().Show();
             //new WIN_GETFIRSTMOG().Show();
-            //new NEWPASSWORD().Show();
+            new NEWPASSWORD().Show();
             //new F_MENU_DATE("CROS").Show();
             //CL_MenuManager.OpenWinMenu(CL_MenuManager.WinNameType.PGET_LST_SEARCH, this);
             //new WIN_F_NEWYEAR().Show();
