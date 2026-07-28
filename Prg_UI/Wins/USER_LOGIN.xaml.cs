a57240e9ebad37a3191ae7ff22e06d2a29e719ba
mojtabahakimian <72439342+mojtabahakimian@users.noreply.github.com>
2026-07-28 23:11:58 +0330

    45

--- Prg_UI/Wins/USER_LOGIN.xaml.cs
+++ Prg_UI/Wins/USER_LOGIN.xaml.cs
@@ -371,7 +371,7 @@ namespace Prg_UI.Wins
             //Baseknow.USERCOD = 56; Baseknow.UUSER = "Mr-kazemi";
             //Baseknow.USERCOD = 116; Baseknow.UUSER = "Mr-pakzaban";
             Baseknow.USERCOD = 35; Baseknow.UUSER = "كنترل";
-            Baseknow.USERCOD = 5; Baseknow.UUSER = "كنترل";
+            //Baseknow.USERCOD = 5; Baseknow.UUSER = "كنترل";
             //Baseknow.USERCOD = 78; Baseknow.UUSER = "Controller";
             //Baseknow.USERCOD = 179; Baseknow.UUSER = "واحد تولید یزدویزیتوری";
 
