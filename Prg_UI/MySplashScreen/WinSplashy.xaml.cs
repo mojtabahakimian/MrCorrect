2afcc913d317555256fc162b1f7029535096f351
mojtabahakimian <72439342+mojtabahakimian@users.noreply.github.com>
2026-08-09 14:46:55 +0330

    42

--- Prg_UI/MySplashScreen/WinSplashy.xaml.cs
+++ Prg_UI/MySplashScreen/WinSplashy.xaml.cs
@@ -9,29 +9,49 @@ namespace Prg_UI.MySplashScreen
         {
             InitializeComponent();
 
-            //<Image x:Name="myg" Margin="-9,8,4,5" Visibility="Hidden" gifplayer:AnimationBehavior.RepeatBehavior="2x" gifplayer:AnimationBehavior.AutoStart="True" gifplayer:AnimationBehavior.SourceUri="/UiDrive/IMGS/SplashHandwrite.gif" Stretch="UniformToFill"/>
-            if (true) //GeneralOptionManager.IsRDPMode
+            bool isRdp = true; // GeneralOptionManager.IsRDPMode
+
+            if (isRdp)
             {
-                myg.Visibility = Visibility.Collapsed;
-                myg = null;
+                // حالت سبک برای Remote Desktop
+                NormalContent.Visibility = Visibility.Collapsed;
+                Circle1.Visibility = Visibility.Collapsed;
+                Circle2.Visibility = Visibility.Collapsed;
+
+                if (myg != null)
+                {
+                    myg.Visibility = Visibility.Collapsed;
+                    myg.Source = null;
+                    myg = null;
+                }
+
                 RDP_LABEL.Visibility = Visibility.Visible;
+
+                // حذف سایه برای عملکرد بهتر روی RDP
+                MainBorder.Effect = null;
             }
         }
+
         public void LoadComplete()
         {
-            Dispatcher.Invoke(() => { if (myg is not null) myg.Source = null; });
+            Dispatcher.Invoke(() =>
+            {
+                if (myg != null)
+                    myg.Source = null;
+            });
 
             Dispatcher.InvokeShutdown();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            if (myg is not null)
+            if (myg != null)
             {
                 myg.Visibility = Visibility.Visible;
             }
         }
     }
+
     public interface ISplashScreen
     {
         void LoadComplete();
